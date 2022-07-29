5271633b2fcab672fa556dabb83c8b9b23b1dac2
Wen1i <104118101+Wen1i@users.noreply.github.com>
2022-07-29 21:31:37 +0800

    Update sm3_opt.c

--- Project05-optimization-of-SM3-main/sm3_opt.c
+++ Project05-optimization-of-SM3-main/sm3_opt.c
@@ -11,7 +11,7 @@
 #include <chrono>
 #include <time.h>
 #include <stdlib.h>
-#include "sm3_opt.h"
+#include "sm4.h"
 
 using namespace std;
 
@@ -27,11 +27,11 @@ static const int endianTest = 1;
 
 #define LeftRotate(word, bits) ( (word) << (bits) | (word) >> (32 - (bits)) )
 
-unsigned int *ReverseWord(unsigned int *word)
+unsigned int* ReverseWord(unsigned int* word)
 {
-	unsigned char *byte, temp;
+	unsigned char* byte, temp;
 
-	byte = (unsigned char *)word;
+	byte = (unsigned char*)word;
 	temp = byte[0];
 	byte[0] = byte[3];
 	byte[3] = temp;
@@ -55,9 +55,9 @@ unsigned int T(int i)
 
 void caculT() {
 	for (int i = 0; i < 64; i++) {
-		t[i] = LeftRotate(T(i),i);
+		t[i] = LeftRotate(T(i), i);
 	}
-	return ;
+	return;
 }
 
 /*FF*/
@@ -95,7 +95,7 @@ unsigned int P1(unsigned int X)
 }
 
 
-void SM3Init(SM3::SM3Context *context) {
+void SM3Init(SM3::SM3Context* context) {
 	context->intermediateHash[0] = 0x7380166F;
 	context->intermediateHash[1] = 0x4914B2B9;
 	context->intermediateHash[2] = 0x172442D7;
@@ -108,22 +108,22 @@ void SM3Init(SM3::SM3Context *context) {
 
 
 
-void one_round(int i,unsigned int &A, unsigned int &B, unsigned int &C, unsigned int &D,
-	unsigned int &E, unsigned int &F, unsigned int &G, unsigned int &H, unsigned int W[68],SM3::SM3Context *context)
+void one_round(int i, unsigned int& A, unsigned int& B, unsigned int& C, unsigned int& D,
+	unsigned int& E, unsigned int& F, unsigned int& G, unsigned int& H, unsigned int W[68], SM3::SM3Context* context)
 {
 	unsigned int SS1 = 0, SS2 = 0, TT1 = 0, TT2 = 0;
 
 	if (i < 12) {
-		W[i+4] = *(unsigned int *)(context->messageBlock + (i+4) * 4);
+		W[i + 4] = *(unsigned int*)(context->messageBlock + (i + 4) * 4);
 		if (IsLittleEndian())
 			ReverseWord(W + i + 4);
 	}
 	else {
 		/*P1*/
-		W[i+4] = ((W[i - 12] ^ W[i - 5] ^ LeftRotate(W[i + 1], 15)) ^ LeftRotate((W[i - 12] ^ W[i - 5] ^ LeftRotate(W[i + 1], 15)), 15) ^ LeftRotate((W[i - 12] ^ W[i - 5] ^ LeftRotate(W[i + 1], 15)), 23))
+		W[i + 4] = ((W[i - 12] ^ W[i - 5] ^ LeftRotate(W[i + 1], 15)) ^ LeftRotate((W[i - 12] ^ W[i - 5] ^ LeftRotate(W[i + 1], 15)), 15) ^ LeftRotate((W[i - 12] ^ W[i - 5] ^ LeftRotate(W[i + 1], 15)), 23))
 			^ LeftRotate(W[i - 9], 7)
 			^ W[i - 2];
-		}
+	}
 
 	TT2 = LeftRotate(A, 12);
 	TT1 = TT2 + E + t[i];
@@ -137,7 +137,7 @@ void one_round(int i,unsigned int &A, unsigned int &B, unsigned int &C, unsigned
 	H = H ^ LeftRotate(H, 9) ^ LeftRotate(H, 17);
 }
 
-void SM3ProcessMessageBlock(SM3::SM3Context *context)
+void SM3ProcessMessageBlock(SM3::SM3Context* context)
 {
 	int i;
 	unsigned int W[68];
@@ -147,7 +147,7 @@ void SM3ProcessMessageBlock(SM3::SM3Context *context)
 
 	for (i = 0; i < 4; i++)
 	{
-		W[i] = *(unsigned int *)(context->messageBlock + i * 4);
+		W[i] = *(unsigned int*)(context->messageBlock + i * 4);
 		if (IsLittleEndian())
 			ReverseWord(W + i);
 		//        printf("%d: %x\n", i, W[i]);
@@ -161,12 +161,12 @@ void SM3ProcessMessageBlock(SM3::SM3Context *context)
 	F = context->intermediateHash[5];
 	G = context->intermediateHash[6];
 	H = context->intermediateHash[7];
-	for (i = 0; i <= 60; i+=4)
+	for (i = 0; i <= 60; i += 4)
 	{
 		one_round(i, A, B, C, D, E, F, G, H, W, context);
-		one_round(i+1, D, A, B, C, H, E, F, G, W, context);
-		one_round(i+2, C, D, A, B, G, H, E, F, W, context);
-		one_round(i+3, B, C, D, A, F, G, H, E, W, context);
+		one_round(i + 1, D, A, B, C, H, E, F, G, W, context);
+		one_round(i + 2, C, D, A, B, G, H, E, F, W, context);
+		one_round(i + 3, B, C, D, A, F, G, H, E, W, context);
 
 	}
 	context->intermediateHash[0] ^= A;
@@ -179,13 +179,13 @@ void SM3ProcessMessageBlock(SM3::SM3Context *context)
 	context->intermediateHash[7] ^= H;
 }
 
-unsigned char *SM3::SM3Calc(const unsigned char *message,
+unsigned char* SM3::SM3Calc(const unsigned char* message,
 	unsigned int messageLen, unsigned char digest[SM3_HASH_SIZE])
 {
 	SM3::SM3Context context;
 	unsigned int i, remainder, bitLen;
 
-	SM3Init(&context);ֵ
+	SM3Init(&context);
 	hash_all = messageLen / 64 + 1;
 	remainder = messageLen % 64;
 	if (remainder > 55) {
@@ -232,12 +232,12 @@ unsigned char *SM3::SM3Calc(const unsigned char *message,
 	return digest;
 }
 
-std::vector<uint32_t> SM3::call_hash_sm3(char *filepath)
+std::vector<uint32_t> SM3::call_hash_sm3(char* filepath)
 {
 	std::vector<uint32_t> hash_result(32, 0);
 	std::ifstream infile;
 	uint32_t FILESIZE = 0;
-	unsigned char * buffer = new unsigned char[MAXSIZE];
+	unsigned char* buffer = new unsigned char[MAXSIZE];
 	unsigned char hash_output[32];
 	struct _stat info;
 	_stat(filepath, &info);
@@ -247,9 +247,8 @@ std::vector<uint32_t> SM3::call_hash_sm3(char *filepath)
 	auto start = std::chrono::high_resolution_clock::now();
 	SM3::SM3Calc(buffer, FILESIZE, hash_output);
 	auto end = std::chrono::high_resolution_clock::now();
-	std::cout << "in millisecond time:";
 	std::chrono::duration<double, std::ratio<1, 1000>> diff = end - start;
-	std::cout << "Time is " << diff.count() << " ms\n";
+	std::cout << "用时 " << diff.count() << " ms\n";
 
 	hash_result.assign(&hash_output[0], &hash_output[32]);
 
@@ -268,28 +267,32 @@ void CreatTxt(char* pathName, int length)
 	ofstream fout(pathName);
 	char char_list[] = "abcdefghijklmnopqrstuvwxyz";
 	int n = 26;
-	if (fout) { 
+	if (fout) {
 		for (int i = 0; i < length; i++)
 		{
 			fout << char_list[rand() % n];
 		}
 
-		fout.close(); 
+		fout.close();
 	}
 }
 
 int main() {
 	char filepath[] = "test.txt";
+	cout << "数据规模：" << MAX_CHAR_NUM << "字节" << endl;
 	CreatTxt(filepath, MAX_CHAR_NUM);
 	std::vector<uint32_t> hash_result;
 	caculT();
 	hash_result = SM3::call_hash_sm3(filepath);
-	for (int i = 0; i < 32; i++) {
+
+	/*
+	* 	for (int i = 0; i < 32; i++) {
 		std::cout << std::hex << std::setw(2) << std::setfill('0') << hash_result[i];
 		if (((i + 1) % 4) == 0) std::cout << " ";
 	}
-	std::cout << std::endl;
+	*/
 
+	std::cout << std::endl;
 	double rate = progress();
 	return 0;
 }
